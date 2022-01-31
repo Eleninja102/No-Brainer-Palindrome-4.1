12a75fd63eacd81b460d97688b0e301113d98816
Coleton Watt <electricninja102@gmail.com>
2022-01-31 16:42:06 -0700

    Initial Commit

---
+++ No-Brainer: Palindrome 4.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No-Brainer: Palindrome 4.1
+//
+//  Created by Coleton Watt on 1/31/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
